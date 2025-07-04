294cc0cf758c404adfee914175bdec144be84e20
adam-ibnu-ridwan <adamibnuridwanair@gmail.com>
2025-07-04 15:46:36 +0700

    Initial

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+// const port = 1234;
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const mahasiswa = [
+    {
+      nama: "Adam Ridwan",
+      email: "xxxxxx@gmail.com",
+      jurusan: "I'dad Lughowy",
+    },
+    {
+      nama: "Ibnu Ridwan",
+      email: "yyyyyyy@gmail.com",
+      jurusan: "Syariah",
+    },
+    {
+      nama: "Ridwan",
+      email: "zzzzzzz@gmail.com",
+      jurusan: "Lughoh",
+    },
+  ];
+
+  res.render("index", {
+    title: "Halaman Home",
+    nama: "Adam",
+    layout: "layouts/main-layout",
+    mahasiswa,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Halaman Contact",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.use("/", (req, res) => {
+  res.sendStatus(404).send("<h1>404 Page Not Found!</h1>");
+});
+
+// app.listen(port, () => {
+//   console.log(`The app is listening at http://localhost:${port}`);
+// });
+
+module.exports = app;
