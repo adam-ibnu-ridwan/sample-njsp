405bd749d0c92963ace3435129068536f17125d0
adam-ibnu-ridwan <adamibnuridwanair@gmail.com>
2025-07-05 17:37:48 +0700

    hahay

--- api/index.js
+++ api/index.js
@@ -30,30 +30,13 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Halaman Home",
     nama: "Adam", // data bebas
+    layout: "layouts/main-layout",
     mahasiswa,
   });
 });
 
-// app.get("/about", (req, res) => {
-//   res.render("about", {
-//     title: "Halaman About",
-//     layout: "layouts/main-layout",
-//   });
-// });
-
-// app.get("/contact", (req, res) => {
-//   res.render("contact", {
-//     title: "Halaman Contact",
-//     layout: "layouts/main-layout",
-//   });
-// });
-
 app.use("/", (req, res) => {
   res.send("<h1>404 Page Not Found!</h1>");
 });
-
-// app.listen(1234, () => {
-//   console.log(`The app is listening at http://localhost:${1234}`);
-// });
 
 module.exports = app;
